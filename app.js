5dff066449962b96d33d278e9e5613da2ebb24d3
jin.wang <jin.wang@reallytek.com>
2016-07-07 20:04:56 +0800

    update code

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var express = require('express');
 // 建立 express 实例
 var app = express();
 
+app.use('/resources', express.static('public'));
+
 app.get('/', function (req, res) {
   // 从 req.query 中取出我们的 q 参数。
   // 如果是 post 传来的 body 数据，则是在 req.body 里面，不过 express 默认不处理 body 中的信息，需要引入 https://github.com/expressjs/body-parser 这个中间件才会处理，这个后面会讲到。
@@ -16,7 +18,7 @@ app.get('/', function (req, res) {
   // utility 的 github 地址：https://github.com/node-modules/utility
   // 里面定义了很多常用且比较杂的辅助方法，可以去看看
 
-  res.send(q);
+  res.sendFile(__dirname + '/views/home.html');
 });
 
 app.listen(process.env.PORT || 3000, function (req, res) {
