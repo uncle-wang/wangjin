be73412e67349f09b20f9e067dc5552c92672ed2
jin.wang <jin.wang@reallytek.com>
2016-07-08 16:49:52 +0800

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
 var jade = require('jade');
+var pg = require('pg');
 
 var app = express();
 app.use('/resources', express.static('public'));
@@ -9,15 +10,29 @@ app.set('views', './views');
 app.set('view engine', 'jade');
 app.engine('jade', jade.__express);
 app.get('/', function (req, res) {
+	var str = '';
+	pg.defaults.ssl = true;
+	pg.connect(process.env.DATABASE_URL, function(err, client) {
+		if (err) throw err;
+		console.log('Connected to postgres! Getting schemas...');
 
-	var param = req.query.param;
+		client
+		.query('SELECT * FROM n_user;')
+		.on('row', function(row) {
+			str += JSON.stringify(row);
+		});
+		res.send(str);
+	});
+
+/*	var param = req.query.param;
 	if (param !== '' && param !== undefined) {
 		
 		app.locals.title = param;
 	}
 	res.render('test');
 
-/*	superagent.get('http://news.qq.com/zt2015/wxghz/index.htm').end(function(err, content) {
+	superagent.get('http://news.qq.com/zt2015/wxghz/index.htm').end(function(err, content) {
+
 
 		var $ = cheerio.load(content.text);
 		var rows = $('.nrC a .title');
@@ -27,8 +42,9 @@ app.get('/', function (req, res) {
 			var $ele = $(ele);
 			news.push($ele.text());
 			app.locals.news = news;
+			news.push({title: $ele.find('.title').html(), des: $ele.find('p').html()});
 		});
-		res.render('test');
+		res.send(str);
 	});
 */});
 
