fa02bcd3262253e1e77900d9f82cd2434ed0bf28
jin.wang <jin.wang@reallytek.com>
2017-03-30 10:24:29 +0800

    增加/api/接口测试内容

--- controllers/fx.js
+++ controllers/fx.js
@@ -7,6 +7,16 @@ var route = function(app) {
 	.get('/fx', function(req, res) {
 
 		res.sendFile(fp.view('fx'));
+	})
+
+	.get('/api/fx', function(req, res) {
+
+		res.send('fx api')
+	})
+
+	.get('/fx/getTest', function(req, res) {
+
+		console.log('test');
 	});
 
 	return this;
