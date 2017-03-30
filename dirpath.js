b45bd9fffd2ec716a166a979d3542e0dd908f59e
jin.wang <jin.wang@reallytek.com>
2017-03-30 09:52:16 +0800

    修改目录配置逻辑

--- dirpath.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-
-	views: __dirname + '/views/'
-};
---
+++ filepath.js
@@ -0,0 +1,7 @@
+module.exports = {
+
+	view: function(viewName) {
+
+		return __dirname + '/views/' + viewName + '.html';
+	}
+};
