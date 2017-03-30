1daf2defcd6ab67037fdfe18b469ece0a83ec37d
jin.wang <jin.wang@reallytek.com>
2017-03-30 17:55:59 +0800

    category、item增删查接口

--- controllers/fx.js
+++ controllers/fx.js
@@ -16,10 +16,59 @@ var route = function(app) {
 		dw.sendData(res, fx.getCategoryList());
 	})
 
+	.get('/api/fx/addCategory', function(req, res) {
+
+		var categoryName = req.query.categoryName;
+		if (categoryName) {
+			fx.addCategory(categoryName);
+			dw.sendData(res);
+		}
+		else {
+			dw.sendError(res, 'PARAMERROR');
+		}
+	})
+
+	.get('/api/fx/removeCategory', function(req, res) {
+
+		var categoryId = req.query.categoryId;
+		if (categoryId && fx.exsistCategory(categoryId)) {
+			fx.removeCategory(categoryId);
+			dw.sendData(res);
+		}
+		else {
+			dw.sendError(res, 'PARAMERROR');
+		}
+	})
+
+	.get('/api/fx/addItem', function(req, res) {
+
+		var categoryId = req.query.categoryId;
+		var itemName = req.query.itemName;
+		if (categoryId && itemName && fx.exsistCategory(categoryId)) {
+			fx.addItem(categoryId, itemName);
+			dw.sendData(res);
+		}
+		else {
+			dw.sendError(res, 'PARAMERROR');
+		}
+	})
+
+	.get('/api/fx/removeItem', function(req, res) {
+
+		var itemId = req.query.itemId;
+		if (itemId && fx.exsistItem(itemId)) {
+			fx.removeItem(itemId);
+			dw.sendData(res);
+		}
+		else {
+			dw.sendError(res, 'PARAMERROR');
+		}
+	})
+
 	.get('/api/fx/getItemListByCategoryId', function(req, res) {
 
 		var categoryId = req.query.categoryId;
-		if (categoryId) {
+		if (categoryId && fx.exsistCategory(categoryId)) {
 			dw.sendData(res, fx.getItemListByCategoryId(categoryId));
 		}
 		else {
--- modules/fx.js
+++ modules/fx.js
@@ -1,18 +1,85 @@
 var fs = require('fs');
 var fp = require('./../filepath');
 
-var data = JSON.parse(fs.readFileSync(fp.config('fx_data'), 'utf8'));
+// 数据文件路径
+var configFileName = fp.config('fx_data');
 
-var getData = function() {
+// 从文件读取数据
+var data = JSON.parse(fs.readFileSync(configFileName, 'utf8'));
 
-	return data;
+// id自增
+var _selfIncrease = function(id) {
+
+	var prefix = '';
+	var newIdNumber = (Number(id) + 1).toString();
+	var prefixLength = 8 - newIdNumber.length;
+	while (prefixLength > 0) {
+		prefix += '0';
+		prefixLength --;
+	}
+	return prefix + newIdNumber;
 };
 
+// 保存文件
+var _save = function() {
+
+	fs.writeFileSync(configFileName, JSON.stringify(data), 'utf8');
+};
+
+// 查询分类列表
 var getCategoryList = function() {
 
 	return data.categoryList;
 };
 
+// 增加分类
+var addCategory = function(categoryName) {
+
+	var categoryIndex = data.index.category;
+	var categoryId = _selfIncrease(categoryIndex);
+	data.categoryList.push({id: categoryId, name: categoryName});
+	data.index.category = categoryId;
+	_save();
+};
+
+// 删除分类
+var removeCategory = function(categoryId) {
+
+	// 删除当前category下的所有item
+	var itemList = data.itemList;
+	var i = 0;
+	while (i < itemList.length) {
+		if (itemList[i].categoryId === categoryId) {
+			itemList.splice(i, 1);
+		}
+		else {
+			i ++;
+		}
+	}
+	// 删除当前category
+	var categoryList = data.categoryList;
+	for (var j = 0; j < categoryList.length; j ++) {
+		if (categoryList[j].id === categoryId) {
+			categoryList.splice(j, 1);
+			break;
+		}
+	}
+	_save();
+};
+
+// 判断分类是否存在
+var exsistCategory = function(categoryId) {
+
+	var categoryList = data.categoryList;
+	for (var i = 0; i < categoryList.length; i ++) {
+		if (categoryList[i].id === categoryId) {
+			return true;
+		}
+	}
+	return false;
+};
+
+// 查询item列表
 var getItemListByCategoryId = function(categoryId) {
 
 	var itemList = [];
@@ -26,9 +93,50 @@ var getItemListByCategoryId = function(categoryId) {
 	return itemList;
 };
 
+// 删除item
+var removeItem = function(itemId) {
+
+	var itemList = data.itemList;
+	for (var i = 0; i < itemList.length; i ++) {
+		if (itemList[i].id === itemId) {
+			itemList.splice(i, 1);
+			break;
+		}
+	}
+	_save();
+};
+
+// 判断item是否存在
+var exsistItem = function(itemId) {
+
+	var itemList = data.itemList;
+	for (var i = 0; i < itemList.length; i ++) {
+		if (itemList[i].id === itemId) {
+			return true;
+		}
+	}
+	return false;
+};
+
+// 增加item
+var addItem = function(categoryId, itemName) {
+
+	var itemIndex = data.index.item;
+	var itemId = _selfIncrease(itemIndex);
+	data.itemList.push({id: itemId, categoryId: categoryId, name: itemName});
+	data.index.item = itemId;
+	_save();
+};
+
+// return
 module.exports = {
 
-	getData: getData,
+	addItem: addItem,
+	removeItem: removeItem,
+	exsistItem: exsistItem,
+	addCategory: addCategory,
+	removeCategory: removeCategory,
+	exsistCategory: exsistCategory,
 	getCategoryList: getCategoryList,
 	getItemListByCategoryId: getItemListByCategoryId
 };
