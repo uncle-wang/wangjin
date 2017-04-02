a75995ea1861031a9d758a875b322452ff8b906a
wangjin <530206162@qq.com>
2017-04-03 01:22:06 +0800

    视图切换及事件绑定

--- dev/js/fx/main.js
+++ dev/js/fx/main.js
@@ -1,5 +1,5 @@
 // ajax
-var ajax = function(configs) {
+var _ajax = function(configs) {
 
 	var config = {
 
@@ -18,66 +18,119 @@ var ajax = function(configs) {
 	$.ajax(config);
 };
 
-var showCateogries = function() {
+// 视图切换
+var _pagePanel = (function() {
 
-	ajax({
+	var _hideAll = function() {
+		$('.content>*').hide();
+	};
+	return {
 
-		api: 'getCategoryList',
-		success: function(data) {
-			$('.content>*').hide();
-			$('#category_list').show();
+		showCategoryList: function() {
+			_hideAll();
 			$('#content_title').text('项目类别');
-			var categoryList = $('#category');
-			categoryList.empty();
-			for (var i = 0; i < data.length; i ++) {
-				var item = $('<li class="item"><div class="item-name">' + data[i].name + '</div></li>');
-				categoryList.append(item);
-			}
-			categoryList.children('li').click(function() {
-				var categoryId = data[$(this).index()].id;
-				showItems(categoryId);
-			});
-		}
-	});
-};
-
-var showItems = function(categoryId) {
-
-	ajax({
-
-		api: 'getItemListByCategoryId',
-		data: {categoryId: categoryId},
-		success: function(data) {
-			$('.content>*').hide();
+			$('#category_list').show();
+		},
+		showItemList: function() {
+			_hideAll();
+			$('#content_title').text('项目列表');
 			$('#item_list').show();
-			$('#content_title').text('项目列表');
-			var itemList = $('#item');
-			itemList.empty();
-			for (var i = 0; i < data.length; i ++) {
-				var item = $('<li class="item"><div class="item-name">' + data[i].name + '</div></li>');
-				itemList.append(item);
+		},
+		showItemDetail: function() {
+			_hideAll();
+			$('#content_title').text('项目详情');
+			$('#item_detail').show();
+		}
+	};
+}());
+
+// category
+var category = (function() {
+
+	var showCacheList = function() {
+
+		_pagePanel.showCategoryList();
+	};
+
+	var showList = function() {
+
+		_ajax({
+			api: 'getCategoryList',
+			success: function(data) {
+				var categoryList = $('#category');
+				categoryList.empty();
+				for (var i = 0; i < data.length; i ++) {
+					var item = $('<li class="item" id="category_' + data[i].id + '"><div class="item-name">' + data[i].name + '</div></li>');
+					categoryList.append(item);
+				}
+				categoryList.children('li').click(function() {
+					var categoryId = $(this).attr('id').substr(9);
+					Item.showList(categoryId);
+				});
+				_pagePanel.showCategoryList();
 			}
-			itemList.children('li').click(function() {
-				var itemId = data[$(this).index()].id;
-				showItem(itemId);
-			});
+		});
+	};
+
+	return {
+
+		showCacheList: showCacheList,
+		showList: showList
+	};
+}());
+
+// item
+var Item = (function() {
+
+	var items = [];
+
+	var _getItem = function(itemId) {
+
+		for (var i = 0; i < items.length; i ++) {
+			var item = items[i];
+			if (item.id === parseInt(itemId)) {
+				return item;
+				break;
+			}
 		}
-	});
-};
+		return null;
+	};
 
-var showItem = function(itemId) {
+	var showCacheList = function() {
 
-	ajax({
+		_pagePanel.showItemList();
+	};
 
-		api: 'getItem',
-		data: {itemId: itemId},
-		success: function(data) {
-			$('.content>*').hide();
-			console.log(data);
-			$('#content_title').text('项目列表');
-		}
-	});
-};
+	var showList = function(categoryId) {
+
+		var self = this;
+		_ajax({
+			api: 'getItemListByCategoryId',
+			data: {categoryId: categoryId},
+			success: function(data) {
+				var itemList = $('#item');
+				itemList.empty();
+				items = data;
+				for (var i = 0; i < data.length; i ++) {
+					var item = $('<li class="item" id="item_' + data[i].id + '"><div class="item-name">' + data[i].name + '</div></li>');
+					itemList.append(item);
+				}
+				itemList.children('li').click(function() {
+					var itemId = $(this).attr('id').substr(5);
+					var item = _getItem(itemId);
+					alert(JSON.stringify(item));
+				});
+				_pagePanel.showItemList();
+			}
+		});
+	};
+
+	return {
+
+		showCacheList: showCacheList,
+		showList: showList
+	};
+}());
 
 // 初始化时间
 var initCopyright = function() {
@@ -89,6 +142,11 @@ var initCopyright = function() {
 
 (function() {
 
-	showCateogries();
+	$('#item_list .btn-close').click(function() {
+
+		category.showCacheList();
+	});
+
+	category.showList();
 	initCopyright();
 }());
